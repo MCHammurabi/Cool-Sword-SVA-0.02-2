8603d236b1bec1af88beccbe5ede216a6de7a84d
MCHammurabi <v.exaltare@gmail.com>
2014-11-30 06:12:14 -0700

    Initial Commit

---
+++ Cool Sword SVA 0.02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cool Sword SVA 0.02
+//
+//  Created by Victor Cook on 11/30/14.
+//  Copyright (c) 2014 Victor Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
